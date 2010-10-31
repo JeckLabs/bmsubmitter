def17a96bc1606bf4bac8beb97748e694e2b88a2
Jeck <i@jeck.ru>
2010-10-31 15:08:02 +0000

    Добавлена проверка на существование модулей при возвращении результата из get метода.
    git-svn-id: file:///Users/Jeck/Code/php/bmsubmitter/trunk@3 061fbcf0-d383-48d6-9823-cd146e9b6700

--- libs/groups.php
+++ libs/groups.php
@@ -6,6 +6,19 @@ class Groups extends Storage {
 		$modules = array_map('mb_strtolower', $modules);
 		parent::set($group, $modules);
 	}
+	function get($group = null) {
+		global $Modules;
+		
+		$modules = parent::get($group);
+		if (!is_null($group)) {
+			foreach ($modules as $key => $moduleName) {
+				if (!isset($Modules[$moduleName])) {
+					unset($modules[$key]);
+				}
+			}
+		}
+		return $modules;
+	}
 }
 
 ?>
